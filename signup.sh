5232983de87af41945bf66166460561148d539b2
Yargo <yargo@gigof>
2018-10-28 17:23:19 +0000

    begin signup script for new user account applications

---
+++ signup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# anonymous signup script, may be run as login shell
+submit=$HOME/submissions.txt
+lockf=$submit.lock
+
+cat <<EOH
+
+Welcome to the application process for new accounts!
+
+EOH
