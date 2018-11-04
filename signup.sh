7f3535b93443523f96681c8ed0ee41a492a4a7d6
Yargo <yargo@gigof>
2018-11-04 21:47:29 +0000

    add input of pubkey

--- signup.sh
+++ signup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # anonymous signup script, may be run as login shell
+
+# submissions log
 submit=$HOME/submissions.txt
 lockf=$submit.lock
-grace=15
+
+# timeout/seconds
+grace=300
+# minimal length of new user name
+minlen=4
 
 # cleanup after timeout
 timeout() {
@@ -42,7 +48,7 @@ You have $grace seconds to finish this;
 after that time, the process will abort.
 
 Please enter your desired username!
-(4 to 8 characters, only letters and numbers,
+($minlen to 8 characters, only letters and numbers,
 first character must be a letter)
 EOH
 
@@ -55,10 +61,53 @@ do
  newname=`echo "$newname" | tr A-Z a-z | tr -c -d '0-9a-z'`
  newname=`echo "$newname" | sed -e 's/^[0-9]*//;s/\(........\).*/\1/'`
  echo "your input is sanitized as follows: $newname"
- if test ${#newname} -lt 4
+ if test ${#newname} -lt $minlen
  then echo "too short, illegal!"
   newname=''
  fi
 done
+
+cat <<EOT
+
+Please enter an ssh public key string (NOT private key)
+which will allow you to login with the username $newname.
+
+You may enter it on several lines, which will be concatenated.
+End your entry with a single '.' (dot) on a line.
+You can start over by entering 'X' (capital X) on a line.
+
+EOT
+
+pubkey=''
+pubpart=''
+while test "$pubpart" != "."
+do read pubpart
+ case $pubpart in
+  X) pubkey=''
+   echo "starting over with empty buffer"
+   ;;
+  .) echo "finished" ;;
+  *) pubkey="$pubkey$pubpart" ;;
+ esac
+done
+
+cat <<EOT
+your entered key:
+---
+$pubkey
+---
+
+Thank you for your submission!
+Please wait at least 1 day before attempting to log in.
+EOT
+
+cat <<EOT >> $submit
+
+# `date -u`
+ newname=$newname
+ pubkey=$pubkey
+
+-----
+EOT
 
 kill $wpid
