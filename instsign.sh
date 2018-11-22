4481618717ace510e39db4efd620eb8c6f85c67e
Yargo <yargo@gigof>
2018-11-22 07:51:16 +0000

    contd instsign

--- instsign.sh
+++ instsign.sh
@@ -1,2 +1,41 @@
 #!/bin/sh
 # install signup account
+
+# account name
+newuser=new
+# options for useradd
+# shell must be absolute path into installation directory of gigof
+addopts="--shell /var/local/gigof/signup.sh --create-home --gid nogroup --system"
+
+if test `id -u` -gt 0
+then echo 'must be root or run with sudo, aborting!'
+ exit 9
+fi
+if id $newuser >/dev/null 2>&1
+then echo "$newuser already exists, aborting!"
+ exit 8
+fi
+
+# find useradd
+usradd=`command -v useradd`
+if test "$usradd" = ""
+then echo :: cannot find useradd, aborting!
+ exit 6
+fi
+
+if test -x $usradd
+then
+ echo ":: creating system user '$newuser'"
+# specific Linux!
+ $usradd $addopts $newuser
+ sync
+else
+ echo :: missing useradd tool, aborting
+ exit 5
+fi
+
+cat <<EOT
+$newuser is installed: `id $newuser`
+please set password according to your system requirements!
+done.
+EOT
